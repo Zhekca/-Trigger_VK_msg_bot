54d9742f983f496783be08d9fd5352a63f6f0b5d
Zhekca <75063786+Zhekca@users.noreply.github.com>
2025-11-17 17:03:23 +0300

    main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import requests
 
-TELEGRAM_TOKEN = os.getenv("8101896389:AAEMwjBctSEIKbcNL0ZdvybJQ7qQbtSCqOY")
-VK_TOKEN = os.getenv("vk1.a.zmOnoN3GVndI-X6SavZkIaEhrXQK3GAkIVo2uqJdvfI43h9BFgU6SB_7qTQK6aawGm0qKmpE7lYJndxi1WPaHMBbutlMNDug6zm0-cy_l8ulkFq0nT3ejwqxwrybrhJc9_kC_lwKO6qldJ5KQRTqisZzCDpjlX5RipmICW56s-slgreLlcksFq0uY80P_IAbdjfABBhtuKMMgsy64EYg6A")
-CHAT_IDS = os.getenv("1243372999", "").split(",")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+VK_TOKEN = os.getenv("VK_TOKEN")
+CHAT_IDS = os.getenv("CHAT_IDS", "").split(",")
 
-GROUP_ID = int(os.getenv("211587219")) 
+GROUP_ID = int(os.getenv("GROUP_ID")) 
 
 vk_session = vk_api.VkApi(token=VK_TOKEN)
 longpoll = VkBotLongPoll(vk_session, GROUP_ID)
